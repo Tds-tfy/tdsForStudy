08176cf64f86074f5e71abebfff019b5caa138e7
cosyit <610495444@qq.com>
2016-12-12 03:52:09 +0800

    连不上，你试试

--- liuyichadao/src/com/cosyit/test/Test.java
+++ liuyichadao/src/com/cosyit/test/Test.java
@@ -1,11 +1,13 @@
 package com.cosyit.test;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.cosyit.entity.TeaCommodityInfo;
 import com.cosyit.service.ModelService;
 
 public class Test {
@@ -14,12 +16,22 @@ public class Test {
 		// TODO Auto-generated method stub
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
 		ModelService service=(ModelService) applicationContext.getBean("modelService");
-		List<TeaCommodityInfo> al=service.getInfoByProperties(ModelService.COMMODITY, 1, 3) ;
+/*		List<TeaCommodityInfo> al=service.getInfoByProperties(ModelService.COMMODITY, 1, 3) ;
 		for (TeaCommodityInfo commodityInfo : al) {
 			System.out.println(commodityInfo.getCommodityName());
 		}
-		
-		
+		*/
+		DataSource datasource=(DataSource) applicationContext.getBean("dataSource");
+		System.out.println(datasource);
+		Connection con;
+		try {
+			con = datasource.getConnection();
+			System.out.println(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		
 		
 		
